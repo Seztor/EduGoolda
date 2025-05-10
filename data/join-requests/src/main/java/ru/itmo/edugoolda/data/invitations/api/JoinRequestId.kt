5a1d166aa8eddaaf925442cf1d26f621e8aa473a
Roman <tikhonovroman.al@gmail.com>
2025-05-10 20:57:29 +0300

    Delete unused imports

--- data/join-requests/src/main/java/ru/itmo/edugoolda/data/invitations/api/JoinRequestId.kt
+++ data/join-requests/src/main/java/ru/itmo/edugoolda/data/invitations/api/JoinRequestId.kt
@@ -1,5 +1,4 @@
 package ru.itmo.edugoolda.data.invitations.api
 
-
 @JvmInline
 value class JoinRequestId(val value: String)
--- data/user/src/main/java/ru/itmo/edugoolda/data/user/api/UserInfo.kt
+++ data/user/src/main/java/ru/itmo/edugoolda/data/user/api/UserInfo.kt
@@ -1,8 +1,5 @@
 package ru.itmo.edugoolda.data.user.api
 
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-
 data class UserInfo(
     val id: UserId,
     val name: String,
